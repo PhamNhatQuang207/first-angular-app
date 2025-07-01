dfe4023631dc26928bfe993591726eb981176184
PhamNhatQuang207 <phamquang207207@gmail.com>
2025-07-01 17:28:36 +0700

    css error

---
+++ src/app/housing-location/housing-location.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [CommonModule],
+  templateUrl: './housing-location.html',
+  styleUrls: ['./housing-location.css']
+})
+export class HousingLocation {
+
+}
